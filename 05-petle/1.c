3b0c263d75cde172fd87665c9b6eb97b032246d4
mklimko <mklimko@p110-13.labpk.inf.ug.edu.pl>
2015-11-14 10:43:09 +0100

    petla

---
+++ 05-petle/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// 1 2 4 8 16 32
+
+int main() {
+  int y = 1;
+
+  while (y <= 2010) {
+      printf("%d\n", y);
+      y = y * 2;
+
+    }
+}
---
+++ 05-petle/2pn.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void print_pattern(int ile_gwiazdek) {
+  for(int i = 0; i < ile_gwiazdek - 1; i++) {
+    printf("* ");
+  }
+  printf("*\n");
+  for(int i = 0; i < ile_gwiazdek; i++) {
+    printf(" *");
+  }
+  printf("\n");
+}
+
+int main() {
+  int n = 4;
+
+    for(int i = 0; i < n; i++) {
+      print_pattern(8);
+  }
+}
