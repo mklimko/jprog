03485030eca9031411b83297b540af3bfa1451e4
mklimko <mklimko@p110-13.labpk.inf.ug.edu.pl>
2015-11-14 12:25:38 +0100

    3.c

---
+++ 05-petle/3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int a, b;
+
+  printf("Podaj dwie liczby ");
+  scanf("%d %d", &a, &b);
+
+  if (a > b) {
+    printf("Pierwsza liczba musi być mniejsza od drugiej.\n");
+  }
+
+
+for (a = a - 1; a < b; a++) {
+  printf("%d\n", a + 1);
+}
+
+}
