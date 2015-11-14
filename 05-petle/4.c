7e0bfdfca24941b93739d11b084fde031ce27108
mklimko <mklimko@p110-13.labpk.inf.ug.edu.pl>
2015-11-14 12:45:01 +0100

    4.c

---
+++ 05-petle/4.c
@@ -0,0 +1,19 @@
+//
+
+#include <stdio.h>
+
+int main() {
+
+int a;
+
+  printf("Podaj liczbę większą od 0: ");
+  scanf("%d", &a);
+
+if (a <= 0) {
+  printf("Liczba musi byc większa od 0.\n");
+  }
+
+// pętla while?
+
+
+}
