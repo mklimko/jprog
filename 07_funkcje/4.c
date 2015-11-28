1752a3f52f2cf16bc0f0d13f036590c5b568a189
mklimko <mklimko@p110-13.labpk.inf.ug.edu.pl>
2015-11-28 12:32:12 +0100

    4.c

---
+++ 07_funkcje/4.c
@@ -0,0 +1,3 @@
+// Napisz funkcję, która po wczytaniu liczby całkowitej wypisze jej cyfry
+// zaczynając od ostatniej i kończąc na pierwszej. Na przykład po wczytaniu
+// liczby '1410' funkcja powinna wypisać '0141'.
