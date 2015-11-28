f7b2cf5ec1d882ed90c40fcfdf6164b6c1fa03d6
mklimko <mklimko@p110-13.labpk.inf.ug.edu.pl>
2015-11-28 12:19:32 +0100

    3.c

---
+++ 07_funkcje/3.c
@@ -0,0 +1,33 @@
+// Liczba doskonała to liczba, która jest sumą podzielników od niej mniejszych.
+// Na przykład 6 jest liczbą doskonałą ponieważ:
+// 1 + 2 + 3 = 6
+// Napisz funkcję, która sprawdza czy podana liczba jest doskonała.
+// Użyj tej funkcji do wypisania wszystkich liczb doskonałych mniejszych
+// od 10 000.
+// Liczby doskonałe: 6, 28, 496, 8128, 33_550_336, 8_589_869_056
+
+#include <stdio.h>
+
+int is_perfect(size_t n) {
+  int suma_podzielnikow = 0;
+
+for (size_t i = 1; i < n; i++) {
+  if (n % i == 0) {
+    suma_podzielnikow += i;
+  }
+}
+
+  return suma_podzielnikow == n;
+}
+
+int main() {
+  size_t max = 10000;
+  // size_t max = 100000000;
+  // jak przyspieszyć proces???
+
+  for (size_t i = 0; i < max; i++) {
+    if (is_perfect(i)) {
+      printf("Liczba %ld jest doskonała\n", i);
+    }
+  }
+}
