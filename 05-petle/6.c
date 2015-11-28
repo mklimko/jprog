54a934df654414c1969e8848b4fc7a7a9673d29f
mklimko <mklimko@p110-13.labpk.inf.ug.edu.pl>
2015-11-28 11:18:23 +0100

    6.c

---
+++ 05-petle/6.c
@@ -0,0 +1,19 @@
+// 1 2 3 4 5 6, min = 1, max = 6
+
+#include <stdio.h>
+
+int main() {
+  int n = 8, min, max;
+  int t[n];
+
+  printf("Podaj %d liczb całkowitych:\n", n);
+  scanf("%d, %d, %d, %d, %d, %d", &t[0], &t[1], &t[2], &t[3], &t[4], &t[5]);
+
+for (size_t i = 0; i < count; i++) {
+  printf("Najmniejsza liczba to: %d\n", min);
+  }
+
+for (size_t i = 0; i < count; i++) {
+  printf("Największa liczba to: %d\n", max);
+  }
+}
