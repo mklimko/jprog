d6661b1fef138bb349eab4cbce3ef5eb45743797
mklimko <mklimko@sigma.ug.edu.pl>
2015-12-22 14:03:55 +0100

    added 9.c

--- 01_budowanie_programow/8.c
+++ 01_budowanie_programow/8.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
 
-  int a, b, c, min, max;
-
-  min =
-  max =
+  int a, b, c;
 
   printf("Wprowadź trzy liczby całkowite: \n");
   scanf("%d %d %d", &a, &b, &c);
 
   printf("Suma wynosi: %d\n", a + b + c);
   printf("Iloczyn wynosi: %d\n", a * b * c);
-  printf("Najmniejsza liczba to: %d\n", min);
-  printf("Największa liczba to: %d\n", max);
+  if (a < b && a < c) {
+    printf("Najmniejsza liczba to: %d\n", a);
+  }
+  else if (b < a && b < c) {
+    printf("Najmniejsza liczba to: %d\n", b);
+  }
+  else {
+    printf("Najmniejsza liczba to: %d\n", c);
+  }
 
+  if (a > b && a > c) {
+    printf("Największa liczba to: %d\n", a);
+  }
+  else if (b > a && b > c) {
+    printf("Największa liczba to: %d\n", b);
+  }
+  else {
+    printf("Największa liczba to: %d\n", c);
+  }
 
-
+  return 0;
 }
